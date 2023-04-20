599aba16ced7e7d651905a59c4f3979daee68487
ChrisTheDaneNerd <107367667+ChrisTheDaneNerd@users.noreply.github.com>
2023-04-20 12:23:12 +0200

    Add files via upload

--- Scripts/Starting_thingy.js
+++ Scripts/Starting_thingy.js
@@ -1,65 +1,73 @@
+const Elforbrug = [];
 var numOne = 0, numTwo = 0, sum = 0;
-numOne = parseInt(document.getElementById("first").value);
-
-function multi(){
-    //function som ganger bruger inputtet sammen med den gennemsnitlige pris for 1 kWh
     
+
+//funktion som generer et cirkel diagram 
+
+function piechart() {
+    //function som ganger bruger inputtet sammen med den gennemsnitlige pris for 1 kWh
+    numOne = parseInt(document.getElementById("first").value);
     
     numTwo = 0.893;
+    numThree = 0.124;
     sum = numOne * numTwo;
+    CO2sum = numOne * numThree;
     document.getElementById("answer").value = sum.toFixed(2);
+    document.getElementById("CO2").value = CO2sum.toFixed(2);
 
-    const Elforbrug = [];
+    
     Elforbrug.push(numOne);
 
     
 
     window.alert(Elforbrug);
-
-}
-
-
-
-
-//funktion som generer et cirkel diagram 
-
-function piechart() {
     
     //variabler
-
-    var num1;
-    num1 = 1600;
-    var sumkoel = 0, sumkom = 0, sumovn = 0, summik = 0, sumvas = 0, sumtoer = 0, sumopv = 0;
-    var numkoel, numkom, numovn, nummik, numvas, numtoer, numopv;
+    
+   
+    var sumkoel = 0, sumkom = 0, sumovn = 0, summik = 0, sumvas = 0, sumtoer = 0, sumopv = 0, sumrest = 0;
+    var numkoel = 0, numkom = 0, numovn = 0, nummik = 0, numvas = 0, numtoer = 0, numopv = 0;
+    var tidkom = 0, tidovn = 0, tidmik = 0, tidvas = 0, tidtoer = 0, tidopv = 0;
     //ændrer bruger inputtet til integer
     numkoel = parseInt(document.getElementById("Køle").value);
     numkom = parseInt(document.getElementById("Komfur").value);
+    tidkom = parseInt(document.getElementById("Komfur-tid").value);
     numovn = parseInt(document.getElementById("Ovn").value);
+    tidovn = parseInt(document.getElementById("Ovn-tid").value);
     nummik = parseInt(document.getElementById("Mikroovn").value);
+    tidmik = parseInt(document.getElementById("Mikro-tid").value);
     numvas = parseInt(document.getElementById("Vaskemaskine").value);
+    tidvas = parseInt(document.getElementById("Vask-tid").value);
     numtoer = parseInt(document.getElementById("Tørretumbler").value);
+    tidtoer = parseInt(document.getElementById("Tørre-tid").value);
     numopv = parseInt(document.getElementById("Opvaskemaskine").value);
+    tidopv = parseInt(document.getElementById("Opvask-tid").value);
+
 
     
     //omregner talene til procenter
     sumkoel= (numkoel/numOne)*100;
-    sumkom= (numkom/numOne)*100;
-    sumovn= (numovn/numOne)*100;
-    summik= (nummik/numOne)*100;
-    sumvas= (numvas/numOne)*100; 
-    sumtoer= (numtoer/numOne)*100; 
-    sumopv= (numopv/numOne)*100; 
+    sumkom= ((numkom*tidkom)/numOne)*100;
+    sumovn= ((numovn*tidovn)/numOne)*100;
+    summik= ((nummik*tidmik)/numOne)*100;
+    sumvas= (((numvas/100)*tidvas)/numOne)*100; 
+    sumtoer= (((numtoer/100)*tidtoer)/numOne)*100; 
+    sumopv= (((numopv/100)*tidopv)/numOne)*100; 
+
+    console.log(sumkoel,sumkom,sumovn,summik,sumvas,sumtoer,sumopv);
+
+    sumrest = 100 - (sumkoel-sumkom-sumovn-summik-sumvas-sumtoer-sumopv);
 
     //opstart på at lave cirkel diagrammet
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         title: {
-            text: "Desktop Search Engine Market Share - 2016"
+            text: "EL fordeling af hvidevare på årligt forbrug af  " + numOne + " kWh"
         },
         data: [{
             //sætter de forskellige variabler til diagrammet
             type: "pie",
-            startAngle: 240,
+            startAngle: 0,
             yValueFormatString: "##0.00\"%\"",
             indexLabel: "{label} {y}",
             dataPoints: [
@@ -70,7 +78,9 @@ function piechart() {
                 {y: summik.toFixed(2), label: "Mikrovn"},
                 {y: sumvas.toFixed(2), label: "Vaskemaskine"},
                 {y: sumtoer.toFixed(2), label: "Tørretumbler"},
-                {y: sumopv.toFixed(2), label: "Opvaskemaskine"}
+                {y: sumopv.toFixed(2), label: "Opvaskemaskine"},
+                {y: sumrest.toFixed(2), label: "Andet"}
+
             ]
         }]
     });
--- Scripts/Tabel.js
+++ Scripts/Tabel.js
@@ -1,5 +1,5 @@
-let solarP = [];
-let Biomass = 0;
+let solarP = []; let Hydrop =[]; let Offshore = []; let Onshore =[]; let OtherRe =[]; let TotalLo =[];
+
 //Get specified amount of data from specified dataset
 fetch('https://api.energidataservice.dk/dataset/ElectricityBalanceNonv?offset=0&filter=%7B%22PriceArea%22:[%22DK1%22]%7D&sort=HourUTC%20DESC&timezone=dk') 
     .then(response => response.json())
@@ -10,11 +10,40 @@ fetch('https://api.energidataservice.dk/dataset/ElectricityBalanceNonv?offset=0&
         //data.records.TotalLoad.forEach(record => console.log(record));
         console.log(data);
         for(let i = 1; i < data.records.length; i++){
-            solarP.pop(data.records[i].Biomass);
-            console.log(solarP[i]);
+            
+        
+            solarP.push(data.records[i].SolarPower);
+            Hydrop.push(data.records[i].HydroPower);
+            Offshore.push(data.records[i].OffshoreWindPower);
+            Onshore.push(data.records[i].OnshoreWindPower);
+            OtherRe.push(data.records[i].OtherRenewable);
+            TotalLo.push(data.records[i].TotalLoad); 
+           
+            
         }
+        console.log(solarP[0]);
+        console.log(Hydrop[0]);
+        console.log(Offshore[0]);
+        console.log(Onshore[0]);
+        console.log(OtherRe[0]);
+        console.log(TotalLo[0]);
+
+       
         //console.groupEnd();
     });
+    function Percent() {
+    
+    var grun = 0;
+    
+    
+
+    grun = ((solarP[0]+Hydrop[0]+Offshore[0]+Onshore[0]+OtherRe[0])/TotalLo[0])*100;
+    
+
+    document.getElementById("grun").value = grun.toFixed(2);
+    
+
+    }
 
     function loadDataIn(data)
     {
